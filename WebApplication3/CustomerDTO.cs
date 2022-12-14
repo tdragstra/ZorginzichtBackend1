6fa318541b83b0176ba696f4df7d025275a9d278
Robin Coes <sigma_fi19@hotmail.com>
2022-12-14 11:29:19 +0100

    op hoop van zegen

---
+++ WebApplication3/CustomerDTO.cs
@@ -0,0 +1,23 @@
+﻿using ZorginzichtBackend.Models;
+
+namespace WebApplication3
+{
+    public class CustomerDTO
+
+    {
+        public int id { get; set; }
+        public int customer_number { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public List<Policy> policies { get; set; }
+        public List<Invoice> invoices { get; set; }
+        public CustomerDTO(Customer customer) {
+            this.name = customer.name;
+            this.email = customer.email;
+            this.policies = customer.policies;
+            this.invoices = customer.invoices;
+            this.id= customer.id;
+            this.customer_number= customer.customer_number;
+        }
+    }
+}
